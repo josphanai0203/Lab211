ab0f46de889108433d487948df2a3edd5907626c
josphanai0203 <Kaidophan37@gmail.com>
2023-09-26 09:14:17 +0700

    123

---
+++ Slot1/normalizeText/src/Main.java
@@ -0,0 +1,12 @@
+
+import controller.NormalizeTextController;
+
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        NormalizeTextController textController = new NormalizeTextController();
+        textController.run();
+    }
+}
